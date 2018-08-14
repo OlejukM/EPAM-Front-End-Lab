c5ea8c026d50438b1e98e459b319d95001f4cce1
OlejukM <olejukmaslo@gmail.com>
2018-08-15 01:56:50 +0300

    hw 6

---
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -0,0 +1,17 @@
+const price = +prompt('Please enter amount of money');
+
+const discount = +prompt('Please enter discount');
+
+const saved = price * discount / 100;
+
+const priceWithDiscount = price - saved;
+
+
+if (price < 0) {
+    console.log('Invalid data');
+} else {
+    console.log(`Price without discount: ${parseFloat(price.toFixed(2))}
+Discount: ${parseFloat(discount.toFixed(2))}%  
+Price with discount: ${parseFloat(priceWithDiscount.toFixed(2))} 
+Saved: ${parseFloat(saved.toFixed(2))}`);
+}
---
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -0,0 +1,16 @@
+const a = +prompt('Enter length of 1st side');
+const b = +prompt('Enter length of 2nd side');
+const angle = +prompt('Enter angle between sides');
+const ang = 180;
+const c = Math.sqrt(a * a + b * b - 2 * a * b * Math.cos(Math.PI / ang * angle));
+const perimeter = a + b + c;
+const p = (a + b + c) / 2;
+const square = Math.sqrt(p * ((p - a) * (p - b) * (p - c)));
+
+if (a < 0 || b < 0 || c < 0) {
+    console.log('Invalid data');
+} else {
+    console.log(`c length: ${parseFloat(c.toFixed(2))}
+Triangle squre: ${parseFloat(square.toFixed(2))}
+Perimeter: ${parseFloat(perimeter.toFixed(2))}`);
+}
