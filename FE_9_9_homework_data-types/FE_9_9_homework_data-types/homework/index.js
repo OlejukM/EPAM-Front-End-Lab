6e3f54f2dd2fab564ab89a13a528f681d1cef620
OlejukM <olejukmaslo@gmail.com>
2018-08-26 18:05:37 +0300

    added last four functions

--- FE_9_9_homework_data-types/FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/FE_9_9_homework_data-types/homework/index.js
@@ -25,7 +25,6 @@ function map(arr, func) {
 // map([2, 5, 8], function(el) { console.log( el + 3) }); => [5, 8, 11]
 
 //4. Write function, which returns filtered array based on function, which passed as a parameter. Reuse function from task 2.
-
 function filter(arr, func) {
     let filteredArray = [];
     forEach(arr, function (el) {
@@ -35,5 +34,41 @@ function filter(arr, func) {
     });
     return filteredArray;
 }
-
 //filter([2, 5, 8], function(el) { return el > 3 }) => [5, 8]
+
+//5. Write function, which returns array of names of people, who are over 18 and their favorite fruit is apple. Reuse functions from task 2 and 3.
+function getAdultAppleLovers(data) {
+    return map(filter(data, (el) => el.age > 18 && el.favoriteFruit === 'apple'), (el) => el.name);
+}
+//getAdultAppleLovers(data) // returns [‘Stein’]
+
+//6. Write function, which returns array of keys of an object.
+function keys(obj) {
+    let arr = [];
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            arr.push(key);
+        }
+    }
+    return arr;
+}
+//keys({keyOne: 1, keyTwo: 2, keyThree: 3}) => [“keyOne”, “keyTwo”, “keyThree”]
+
+//7. Write function, which returns array of values of an object.
+function values(obj) {
+    let arr = [];
+    for (let el in obj) {
+        if (obj.hasOwnProperty(el)) {
+            arr.push(obj[el]);
+        }
+    }
+    return arr;
+}
+//values ({keyOne: 1,keyTwo: 2,keyThree: 3}); => [1, 2, 3];
+
+//8. Write function, which returns formatted date.
+function showFormattedDate(date) {
+    let monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    return `It is ${date.getDate()} of ${monthNames[date.getMonth()]}, ${date.getFullYear()}`;
+}
+//showFormattedDate(new Date('2018-08-27T01:10:00')) => ‘It is 27 of Aug, 2018’
