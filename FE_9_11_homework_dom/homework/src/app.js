23d33376907e315047489af65423c91266dd4a9f
OlejukM <olejukmaslo@gmail.com>
2018-09-02 17:55:12 +0300

    hw10

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,33 +1,60 @@
-let rootNode = document.getElementById('root');
+let removeIcon = '<i class="material-icons" id="del">delete</i>';
+let completeIcon =
+  '<i class="material-icons" id="crop-square">check_box_outline_blank</i>';
+let doneIcon = '<i class="material-icons" id="doneImg">check_box</i>';
+let itemCounter = 0;
+let maxItems = 10;
+let firstNumberOfList = 0;
 let el = document.getElementById('add');
-if(el){
-    el.addEventListener('click', function () {
+if (el) {
+  el.addEventListener('click', function() {
     let value = document.getElementById('item').value;
     if (value) {
- addItemTodo(value); 
+      addItemTodo(value);
+    }
+  });
 }
-});
+
+function removeItem() {
+  let item = this.parentNode;
+  let parent = item.parentNode;
+  itemCounter--;
+  if (itemCounter <= maxItems) {
+    document.getElementById('item').disabled = false;
+    document.getElementById('add').disabled = false;
+  }
+
+  parent.removeChild(item);
+}
+
+function completeItem() {
+  let item = this;
+  item = item.innerHTML = doneIcon;
 }
 
 function addItemTodo(text) {
-    let list = document.getElementById('todo');
+  let list = document.getElementById('todo');
 
-    let item = document.createElement('li');
-    item.innerText = text;
+  let item = document.createElement('li');
+  item.innerText = text;
+  itemCounter++;
+  if (itemCounter >= maxItems) {
+    document.getElementById('item').disabled = true;
+    document.getElementById('add').disabled = true;
+  }
 
-    let buttons = document.createElement('div');
-    buttons.classList.add('buttons');
+  let remove = document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML = removeIcon;
+  remove.addEventListener('click', removeItem);
 
-    let remove = document.createElement('button');
-    remove.classList.add('remove');
+  let complete = document.createElement('button');
+  complete.classList.add('complete');
+  complete.innerHTML = completeIcon;
+  complete.addEventListener('click', completeItem);
 
-    let complete = document.createElement('button');
-    complete.classList.add('complete');
+  item.appendChild(remove);
+  item.appendChild(complete);
 
-    buttons.appendChild(remove);
-    buttons.appendChild(complete);
-    item.appendChild(buttons);
-
-    list.appendChild(item);
+  list.insertBefore(item, list.childNodes[firstNumberOfList]);
 }
-rootNode.appendChild();
