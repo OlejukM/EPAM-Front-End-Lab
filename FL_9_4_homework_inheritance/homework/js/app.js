9aed98456c835dbffd5be72613b9272388a1eb17
OlejukM <olejukmaslo@gmail.com>
2018-10-03 18:02:57 +0300

    return of showPosition fn was changed

--- FL_9_4_homework_inheritance/homework/js/app.js
+++ FL_9_4_homework_inheritance/homework/js/app.js
@@ -1,9 +1,4 @@
 //Task 2
-let coordinatesObject = {
-    x: this.x,
-    y: this.y
-};
-
 function Bot(name, speed, x, y) {
     this.name = name;
     this.speed = speed;
@@ -11,21 +6,25 @@ function Bot(name, speed, x, y) {
     this.y = y;
 }
 
-Bot.prototype.getSpeed = function () {
-    return this.speed;
-}
-
 Bot.prototype.setSpeed = function (speed) {
     this.speed = speed;
 }
 
-Bot.prototype.getCoordinates = function () {
-    return coordinatesObject;
+Bot.prototype.getSpeed = function () {
+    return `Current speed is ${this.speed}`;
 }
 
 Bot.prototype.setCoordinates = function (x, y) {
     this.x = x;
     this.y = y;
+}
+
+Bot.prototype.getCoordinates = function () {
+    let obj = {
+        x: this.x,
+        y: this.y
+    };
+    return obj;
 }
 
 Bot.prototype.move = function (position) {
@@ -43,12 +42,12 @@ Bot.prototype.move = function (position) {
             this.x += this.speed;
             break;
         default:
-            console.log('Where the hell u wanna go ?');
+            console.log('Where the hell you wanna go ?');
     }
 }
 
 Bot.prototype.showPosition = function () {
-    console.log(`I am Bot ${this.name}. I am located at ${this.x}:${this.y}`);
+    return `I am ${this.constructor.name} ${this.name}. I am located at ${this.x}:${this.y}`;
 }
 
 let Botty = new Bot('Betty', 2, 0, 1);
@@ -63,6 +62,15 @@ Botty.showPosition(); // I am Bot 'Betty'. I am located at -2:5.
 Botty.move('up');
 Botty.showPosition(); // I am Bot 'Betty'. I am located at -2:7.
 Botty.move('up');
-Botty.showPosition(); // I am Bot 'Betty'. I am located at -2:9.
-Botty.getCoordinates();
-Botty.setSpeed(10);
+console.log(Botty.showPosition()); // I am Bot 'Betty'. I am located at -2:9.
+Botty.getCoordinates(); // {x: -2, y: 9}.
+Botty.setSpeed(13);
+Botty.getSpeed(); // Current speed is 13.
+
+function Racebot(name, speed, x, y) {
+    Bot.call(this, name, speed, x, y);
+}
+
+Racebot.prototype = Object.create(Bot.prototype);
+Racebot.prototype.constructor = Racebot;
+let Zoom = new Racebot('Lightning', 2, 0, 1);
