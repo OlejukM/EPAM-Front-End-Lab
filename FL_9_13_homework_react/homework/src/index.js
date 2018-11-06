69364489a600181891ed66952134581c66194c71
OlejukM <olejukmaslo@gmail.com>
2018-11-07 00:36:54 +0200

    hw_13

--- FL_9_13_homework_react/homework/src/index.js
+++ FL_9_13_homework_react/homework/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 import './scss/index.scss';
 import { Music } from './music';
 
@@ -9,18 +10,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null,
+      info: [],
       trackLoading: false,
     };
+    this.updateData = this.updateData.bind(this);
   }
 
   componentDidMount() {
     fetch('https://fl-homework-api.firebaseio.com/mozart.json')
       .then(response => response.json())
       .then(data => this.setState({
-        data,
+        info: data,
         trackLoading: true,
       }));
+  }
+
+  updateData(value) {
+    this.props.getMusic(value);
   }
 
   handleIsPlaying() {
@@ -30,7 +36,7 @@ class App extends Component {
   }
 
   render() {
-    const { trackLoading, data } = this.state;
+    const { trackLoading, info } = this.state;
     if (trackLoading === false) {
       return (
         <p>Loading tracks...</p>
@@ -40,16 +46,26 @@ class App extends Component {
       <div>
         <h1>Playlist</h1>
         <ul>
-          { data.map(
+          { info.map(data => (
             <li key={data.id}>
-              <Music updateData={this.updateData} author={data.author} title={data.title} />
-            </li>,
-          )}
+              <Music
+                updateData={this.updateData}
+                author={data.author}
+                poster={data.poster}
+                title={data.title}
+                mp3={data.mp3}
+              />
+            </li>
+          ))}
         </ul>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  getMusic: PropTypes.func.isRequired,
+};
 
 render(
   <App />,
